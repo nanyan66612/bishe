6f3977095cb63dae2a33150ac7c7a850c16869cb
nanyan66612 <nanyan2756@qq.com>
2026-04-26 19:41:14 +0800

    Delete questions directory

--- questions/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Question
-
-class QuestionForm(forms.ModelForm):
-    class Meta:
-        model = Question
-        fields = '__all__'
--- questions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('manage/', views.question_manage, name='question_manage'),
-    path('add/', views.question_add, name='question_add'),
-]
