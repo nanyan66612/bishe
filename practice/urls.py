3d57e1020182a573e86d94afbf8925b69a0b1bdc
nanyan66612 <nanyan2756@qq.com>
2026-04-26 19:40:33 +0800

    Delete practice directory

--- practice/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('list/', views.practice_list, name='practice_list'),
-    path('detail/<int:qid>/', views.practice_detail, name='practice_detail'),
-    path('wrong/', views.wrong_list, name='wrong_list'),
-]
